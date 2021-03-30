d5a33ecc870ae4430171414803e59ebe28add2ac
troyjlee <troyjlee@gmail.com>
2021-03-30 14:22:03 +1100

    added sampling functionality

--- CODE/FTperiod.m
+++ CODE/FTperiod.m
@@ -1,5 +1,6 @@
-N = 60;
-s = 10;
+N = 21^2;
+% s should be the order of x for the x you choose
+s = 6;
 
 T = floor(N/s);
 R = mod(N,s);
@@ -15,3 +16,18 @@ X = X./sqrt(sum(X));
 
 % look at Fourier transform
 Y = fft(X)/sqrt(N);
+
+% sampling
+% sample a random coset state
+j = randi(s);
+
+% sample entry b with prob |Y(b,j)|^2
+probs = abs(Y(:,j)).^2;
+thresh = rand(1);
+% create vector of partial sums of probs
+sum_probs = tril(ones(N))*probs;
+% find the first b with sum_probs(b) > thresh
+[arr,indices] = sort([thresh;sum_probs]);
+disp('The measurement gives index');
+b = find(indices == 1)
+
